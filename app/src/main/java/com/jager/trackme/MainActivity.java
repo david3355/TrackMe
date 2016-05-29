49a94b02cf4a2283e75299a5f570b2e6f6927692
Jäger <david3355@gmail.com>
2016-05-30 01:43:05 +0200

    Main Activity - coordinates
    Coordinate design

--- app/src/main/java/com/jager/trackme/MainActivity.java
+++ app/src/main/java/com/jager/trackme/MainActivity.java
@@ -1,9 +1,12 @@
 package com.jager.trackme;
 
+import android.content.ClipboardManager;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -19,8 +22,9 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 {
        private GoogleMap partial_map;
        private ImageView img_trackingstatus;
-       private TextView txt_trackingstatus;
+       private TextView txt_trackingstatus, txt_position, txt_latitude, txt_longitude;
        private Button btn_start_tracking, btn_stop_tracking;
+       private ImageButton btn_map, btn_settings;
 
 
        @Override
@@ -31,11 +35,22 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
               img_trackingstatus = (ImageView) findViewById(R.id.img_trackingstatus);
               txt_trackingstatus = (TextView)findViewById(R.id.txt_trackingstatus);
+              txt_position = (TextView)findViewById(R.id.txt_position);
+              txt_latitude = (TextView)findViewById(R.id.txt_latitude);
+              txt_longitude = (TextView)findViewById(R.id.txt_longitude);
+
               btn_start_tracking = (Button) findViewById(R.id.btn_trackingstart);
               btn_stop_tracking = (Button) findViewById(R.id.btn_trackingstop);
+              btn_map = (ImageButton) findViewById(R.id.btn_map);
+              btn_settings = (ImageButton) findViewById(R.id.btn_settings);
 
               btn_start_tracking.setOnClickListener(this);
               btn_stop_tracking.setOnClickListener(this);
+              btn_map.setOnClickListener(this);
+              btn_settings.setOnClickListener(this);
+              txt_position.setOnClickListener(this);
+              txt_latitude.setOnClickListener(this);
+              txt_longitude.setOnClickListener(this);
 
               // Obtain the SupportMapFragment and get notified when the map is ready to be used.
               SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
@@ -84,6 +99,16 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
                      case R.id.btn_trackingstop:
                             stopTracking();
                             break;
+                     case R.id.btn_map: startActivity(MapActivity.class);
+                            break;
+                     case R.id.btn_settings: startActivity(SettingsActivity.class);
+                            break;
+                     case R.id.txt_position: copyText(String.format("%s;%s", txt_latitude.getText().toString(), txt_longitude.getText().toString()), "Position");
+                            break;
+                     case R.id.txt_latitude:copyText(txt_latitude.getText().toString(), "Latitude");
+                            break;
+                     case R.id.txt_longitude:copyText(txt_longitude.getText().toString(), "Longitude");
+                            break;
               }
        }
 
@@ -115,5 +140,17 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
               btn_stop_tracking.setVisibility(View.GONE);
               img_trackingstatus.setImageDrawable(getResources().getDrawable(R.drawable.satellite_off));
               txt_trackingstatus.setText(ActivityUtil.getStringRes(this, R.string.tracking_off));
+       }
+
+       private void copyText(String data, String component)
+       {
+              //TODO: copy to clipboard
+              ActivityUtil.popup(String.format("%s [%s] copied to clipboard!", component, data), this);
+       }
+
+       private void startActivity(Class classToStart)
+       {
+              Intent activity = new Intent(this, classToStart);
+              startActivity(activity);
        }
 }
